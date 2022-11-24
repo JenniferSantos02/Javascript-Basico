824fcc344eda81dd4c75363e9ee70bac4240fea2
JenniferSantos02 <51934873+JenniferSantos02@users.noreply.github.com>
2022-11-24 18:54:09 -0500

    Add files via upload

---
+++ 2Sintaxis,-variables-y-palabras-reservadas-de-JS/index.js
@@ -0,0 +1,8 @@
+const lista = ["Jenniffer", 27, false, new Date(1995, 01, 02),
+   {
+      titulo: "The Call of the Wild",
+      autor: "Jack London",
+      fecha: new Date(1990, 06, 01),
+      url: "https://www.amazon.com/-/es/Jack-London/dp/0486264726?ref_=Oct_d_omg_d_10192708011&pd_rd_w=8BQ5J&content-id=amzn1.sym.bd3572e6-1663-4534-8fe2-e56eaeea2af5&pf_rd_p=bd3572e6-1663-4534-8fe2-e56eaeea2af5&pf_rd_r=EGRC6B9X222JE3DK37DH&pd_rd_wg=GakT4&pd_rd_r=89bee45c-ff34-4f27-b377-d0c1e791e0fe&pd_rd_i=0486264726"
+  },
+];
